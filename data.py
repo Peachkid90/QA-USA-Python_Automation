a623ec2229a7c00698a8906a498271b1298fc101
tcduell <tcduell@gmail.com>
2025-07-31 10:51:32 -0700

    4

---
+++ data.py
@@ -0,0 +1,8 @@
+URBAN_ROUTES_URL = 'https://cnt-15c82439-de8c-4400-b613-392b098c64d6.containerhub.tripleten-services.com'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
+
